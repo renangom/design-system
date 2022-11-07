41c5e295a977bb585b2d361fa5d9ead910ecd0ad
Renan <renan23eduardo@gmail.com>
2022-11-07 08:06:07 -0300

    Create text component and use in storybook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import '../src/styles/global.css'
 function App() {
 
   return (
-    <h1 className='font-bold text-2xl text-violet-500'>Hello world</h1>
+    <>
+      <h1 className='font-bold text-2xl text-violet-500'>Hello world</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
+        Enviar
+      </button>
+    </>
   )
 }
 
